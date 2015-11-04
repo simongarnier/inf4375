da777782fd12bdac4f8a09da6fbc310ef903fb85
Simon Garnier <simongarnierg@gmail.com>
2015-11-04 14:57:27 -0500

    renaming package

--- test/inf4375/core_test.clj
+++ test/inf4375/core_test.clj
@@ -1,6 +1,6 @@
-(ns inf4375-simple-server.core-test
+(ns inf4375.core-test
   (:require [clojure.test :refer :all]
-            [inf4375-simple-server.core :refer :all]))
+            [inf4375.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
